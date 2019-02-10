84c81252526ba15c97cdbf54ed6ada91b6b24488
BrianArchibald <brianallenarchibald@gmail.com>
2019-02-09 19:49:24 -0600

    initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/fcadfe61-fd5c-409d-90eb-dca2f43a4460/token";
+const instanceLocator = "v1:us1:fcadfe61-fd5c-409d-90eb-dca2f43a4460";
+
+export { tokenUrl, instanceLocator };
